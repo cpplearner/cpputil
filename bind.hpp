4042891d3469b7d1db87bceef51369e5ad2b7dcb
S. B. Tam <cpplearner@outlook.com>
2016-08-11 17:34:39 +0800

    Create bind.hpp

---
+++ bind.hpp
@@ -0,0 +1,7 @@
+template<class F, class... BoundArgs>
+constexpr /* unspecified */
+  bind(F&& f, BoundArgs&&... bound_args)
+
+template<class R, class F, class... BoundArgs>
+constexpr /* unspecified */
+  bind(F&& f, BoundArgs&&... bound_args)
