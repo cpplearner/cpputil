f0658f029b0681f34d5d12395b96b18d95e0a14c
S. B. Tam <cpplearner@outlook.com>
2017-11-26 00:04:12 +0800

    Update macro.hpp

--- macro.hpp
+++ macro.hpp
@@ -1,3 +1,3 @@
 #define FWDEXPR(...) static_cast<decltype(__VA_ARGS__)&&>(__VA_ARGS__)
 #define RETURNS(...) noexcept(noexcept(__VA_ARGS__)) -> decltype(__VA_ARGS__) { return __VA_ARGS__; }
-#define OVERLOAD_SET(f) [](auto&&... args) RETURNS(f(FWDEXPR(args)...))
+#define OVERLOAD_SET(...) [](auto&&... args) RETURNS(__VA_ARGS__(FWDEXPR(args)...))
