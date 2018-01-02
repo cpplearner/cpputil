0c587ee7f2118ec9eec57b03de25870b1b8062dc
S. B. Tam <cpplearner@outlook.com>
2018-01-03 06:13:29 +0800

    Update variant_visit.hpp

--- variant_visit.hpp
+++ variant_visit.hpp
@@ -11,19 +11,19 @@ struct visit_return_type;
 
 template<class Visitor, class Variant, std::size_t... I>
 struct visit_return_type<Visitor, Variant, std::index_sequence<I...>> {
-    using type = std::common_type_t<std::result_of_t<
-        Visitor(decltype(std::get<I>(std::declval<Variant>())))>...>;
+    using type = std::common_type_t<std::invoke_result_t<
+        Visitor, decltype(std::get<I>(std::declval<Variant>()))>...>;
 };
 
 template<class Visitor, class Variant>
 constexpr decltype(auto) variant_visit(Visitor&& vis, Variant&& var) {
-#define size std::variant_size_v<std::decay_t<Variant>>
+    constexpr auto size = std::variant_size_v<std::decay_t<Variant>>;
     using R = typename visit_return_type<Visitor&&, Variant&&,
         std::make_index_sequence<size>>::type;
     auto f = [&vis, &var, index = var.index()](auto&& self, auto i) -> R {
         // [[assert: &self == &f]];
         static_assert(0 <= i && i <= size);
-        if constexpr (i == size)
+        if constexpr (decltype(i)::value == size)
             throw std::bad_variant_access();
         else if (i == index)
             return (invoke)(static_cast<Visitor&&>(vis),
@@ -32,7 +32,6 @@ constexpr decltype(auto) variant_visit(Visitor&& vis, Variant&& var) {
             return self(self, std::integral_constant<std::size_t, i + 1>{});
         };
     return f(f, std::integral_constant<std::size_t, 0>{});
-#undef size
 }
 
 template<class T, std::size_t> struct wrapper { T elem; };
@@ -48,15 +47,15 @@ constexpr decltype(auto) visit_impl(I, Visitor&& vis,
 
 template<class I, class Visitor, class... Ts, std::size_t... Is,
     class Variant, class... Variants>
-constexpr decltype(auto) visit_impl(I i, Visitor&& vis,
+constexpr decltype(auto) visit_impl(I, Visitor&& vis,
     tuple<wrapper<Ts, Is>...> tpl, Variant&& var, Variants&&... vars)
 {
     auto f = [&vis, &tpl, &vars...](auto&& cur) -> decltype(auto) {
         using T = decltype(cur);
-        return (visit_impl)(std::integral_constant<std::size_t, i + 1>{},
+        return (visit_impl)(std::integral_constant<std::size_t, I::value + 1>{},
             static_cast<Visitor&&>(vis),
-            tuple<wrapper<Ts, Is>..., wrapper<T, i>>{
-                static_cast<wrapper<Ts, Is>>(tpl)..., { static_cast<T>(cur) }
+            tuple<wrapper<Ts, Is>..., wrapper<T, I::value>>{
+                static_cast<wrapper<Ts, Is>&>(tpl)..., { static_cast<T>(cur) }
             },
             static_cast<Variants&&>(vars)...);
     };
