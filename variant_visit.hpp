fc52548fd888eb0adad3bedfee1ca85ae4da0691
S. B. Tam <cpplearner@outlook.com>
2018-01-02 18:35:24 +0800

    Update variant_visit.hpp

--- variant_visit.hpp
+++ variant_visit.hpp
@@ -21,8 +21,8 @@ constexpr decltype(auto) variant_visit(Visitor&& vis, Variant&& var) {
     using R = typename visit_return_type<Visitor&&, Variant&&,
         std::make_index_sequence<size>>::type;
     auto f = [&vis, &var, index = var.index()](auto&& self, auto i) -> R {
-        // [[assert: &self == &f]]
-        // [[assert: 0 <= i && i <= size]]
+        // [[assert: &self == &f]];
+        static_assert(0 <= i && i <= size);
         if constexpr (i == size)
             throw std::bad_variant_access();
         else if (i == index)
@@ -35,23 +35,28 @@ constexpr decltype(auto) variant_visit(Visitor&& vis, Variant&& var) {
 #undef size
 }
 
-template<class T> struct wrapper { T elem; };
+template<class T, std::size_t> struct wrapper { T elem; };
 template<class... Args> struct tuple : Args... {};
 
-template<class Visitor, class... Ts>
-constexpr decltype(auto) visit_impl(Visitor&& vis, tuple<wrapper<Ts>...> tpl) {
-    return static_cast<Visitor&&>(vis)(static_cast<wrapper<Ts>&>(tpl).elem...);
+template<class I, class Visitor, class... Ts, std::size_t... Is>
+constexpr decltype(auto) visit_impl(I, Visitor&& vis,
+    tuple<wrapper<Ts, Is>...> tpl)
+{
+    return static_cast<Visitor&&>(vis)(
+        static_cast<wrapper<Ts, Is>&>(tpl).elem...);
 }
 
-template<class Visitor, class... Ts, class Variant, class... Variants>
-constexpr decltype(auto) visit_impl(Visitor&& vis, tuple<wrapper<Ts>...> tpl,
-    Variant&& var, Variants&&... vars)
+template<class I, class Visitor, class... Ts, std::size_t... Is,
+    class Variant, class... Variants>
+constexpr decltype(auto) visit_impl(I i, Visitor&& vis,
+    tuple<wrapper<Ts, Is>...> tpl, Variant&& var, Variants&&... vars)
 {
     auto f = [&vis, &tpl, &vars...](auto&& cur) -> decltype(auto) {
         using T = decltype(cur);
-        return (visit_impl)(static_cast<Visitor&&>(vis),
-            tuple<wrapper<Ts>..., wrapper<T>>{
-                static_cast<wrapper<Ts>&>(tpl)..., { static_cast<T>(cur) }
+        return (visit_impl)(std::integral_constant<std::size_t, i + 1>{},
+            static_cast<Visitor&&>(vis),
+            tuple<wrapper<Ts, Is>..., wrapper<T, i>>{
+                static_cast<wrapper<Ts, Is>>(tpl)..., { static_cast<T>(cur) }
             },
             static_cast<Variants&&>(vars)...);
     };
@@ -62,6 +67,7 @@ template<class Visitor, class Variant, class... Variants>
 constexpr decltype(auto) variant_visit(Visitor&& vis, Variant&& var,
     Variants&&... vars)
 {
-    return (visit_impl)(static_cast<Visitor&&>(vis), tuple<>{},
+    return (visit_impl)(std::integral_constant<std::size_t, 0>{}, 
+        static_cast<Visitor&&>(vis), tuple<>{},
         static_cast<Variant&&>(var), static_cast<Variants&&>(vars)...);
 }
