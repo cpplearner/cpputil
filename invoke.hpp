130d707bc5ac0cd9f6182b95102a653360a7e71e
S. B. Tam <cpplearner@outlook.com>
2018-05-24 17:10:29 +0800

    Update invoke.hpp

--- invoke.hpp
+++ invoke.hpp
@@ -5,94 +5,95 @@
 
 #include "is_reference_wrapper.hpp"
 
-template<bool /* std::is_member_pointer */>
-struct InvokeSelector {
-    template<class Base, class T, class Derived, class... Args>
-    static constexpr auto call(T Base::*pmf, Derived&& ref, Args&&... args)
-        -> std::enable_if_t<std::is_function_v<T> &&
-                            std::is_base_of_v<Base, std::decay_t<Derived>>,
-    decltype((std::forward<Derived>(ref).*pmf)(std::forward<Args>(args)...))>
-    { return (std::forward<Derived>(ref).*pmf)(std::forward<Args>(args)...); }
-
-    template<class Base, class T, class RefWrap, class... Args>
-    static constexpr auto call(T Base::*pmf, RefWrap&& ref, Args&&... args)
-        -> std::enable_if_t<std::is_function_v<T> &&
-                            is_reference_wrapper_v<std::decay_t<RefWrap>>,
-    decltype((ref.get().*pmf)(std::forward<Args>(args)...))>
-    { return (ref.get().*pmf)(std::forward<Args>(args)...); }
-
-    template<class Base, class T, class Pointer, class... Args>
-    static constexpr auto call(T Base::*pmf, Pointer&& ptr, Args&&... args)
-        -> std::enable_if_t<std::is_function_v<T> &&
-                            !is_reference_wrapper_v<std::decay_t<Pointer>> &&
-                            !std::is_base_of_v<Base, std::decay_t<Pointer>>,
-    decltype(((*std::forward<Pointer>(ptr)).*pmf)(std::forward<Args>(args)...))>
-    { return ((*std::forward<Pointer>(ptr)).*pmf)(std::forward<Args>(args)...); }
-
-    template<class Base, class T, class Derived>
-    static constexpr auto call(T Base::*pmd, Derived&& ref)
-        -> std::enable_if_t<!std::is_function_v<T> &&
-                            std::is_base_of_v<Base, std::decay_t<Derived>>,
-    decltype(std::forward<Derived>(ref).*pmd)>
-    { return std::forward<Derived>(ref).*pmd; }
-
-    template<class Base, class T, class RefWrap>
-    static constexpr auto call(T Base::*pmd, RefWrap&& ref)
-        -> std::enable_if_t<!std::is_function_v<T> &&
-                            is_reference_wrapper_v<std::decay_t<RefWrap>>,
-    decltype(ref.get().*pmd)>
-    { return ref.get().*pmd; }
-
-    template<class Base, class T, class Pointer>
-    static constexpr auto call(T Base::*pmd, Pointer&& ptr)
-        -> std::enable_if_t<!std::is_function_v<T> &&
-                            !is_reference_wrapper_v<std::decay_t<Pointer>> &&
-                            !std::is_base_of_v<Base, std::decay_t<Pointer>>,
-    decltype((*std::forward<Pointer>(ptr)).*pmd)>
-    { return (*std::forward<Pointer>(ptr)).*pmd; }
-};
-
-template<>
-struct InvokeSelector<false> {
+template<class>
+struct invoke_impl {
     template<class F, class... Args>
     static constexpr auto call(F&& f, Args&&... args)
-        -> decltype(std::forward<F>(f)(std::forward<Args>(args)...))
-    { return std::forward<F>(f)(std::forward<Args>(args)...); }
+        noexcept(noexcept(static_cast<F&&>(f)(static_cast<Args&&>(args)...)))
+        -> decltype(static_cast<F&&>(f)(static_cast<Args&&>(args)...))
+    { return static_cast<F&&>(f)(static_cast<Args&&>(args)...); }
 };
 
-template<class F, class... ArgTypes>
-constexpr auto invoke(F&& f, ArgTypes&&... args)
-    -> decltype(
-        InvokeSelector<std::is_member_pointer_v<std::decay_t<F>>>::call(
-            std::forward<F>(f), std::forward<ArgTypes>(args)...)
-        )
+template<class A, class B>
+struct invoke_impl<A B::*> {
+    template<class T,
+        class = std::enable_if_t<std::is_base_of_v<B, std::decay_t<T>>>
+    >
+    static constexpr auto get(T&& t)
+            noexcept(noexcept(static_cast<T&&>(t)))
+            -> decltype(static_cast<T&&>(t))
+    { return static_cast<T&&>(t); }
+
+    template<class T,
+        class = std::enable_if_t<is_reference_wrapper_v<std::decay_t<T>>>
+    >
+    static constexpr auto get(T&& t)
+            noexcept(noexcept(t.get()))
+            -> decltype(t.get())
+    { return t.get(); }
+
+    template<class T,
+        class = std::enable_if_t<!std::is_base_of_v<B, std::decay_t<T>>>,
+        class = std::enable_if_t<!is_reference_wrapper_v<std::decay_t<T>>>
+    >
+    static constexpr auto get(T&& t)
+        noexcept(noexcept(*static_cast<T&&>(t)))
+        -> decltype(*static_cast<T&&>(t))
+    { return *static_cast<T&&>(t); }
+
+    template<class AA, class T, class... Args,
+        class = std::enable_if_t<std::is_function_v<AA>>
+    >
+    static constexpr auto call(AA B::*pmf, T&& t, Args&&... args)
+        noexcept(noexcept(((get)(static_cast<T&&>(t)).*pmf)(static_cast<Args&&>(args)...)))
+        -> decltype(((get)(static_cast<T&&>(t)).*pmf)(static_cast<Args&&>(args)...))
+    { return ((get)(static_cast<T&&>(t)).*pmf)(static_cast<Args&&>(args)...); }
+
+    template<class T>
+    static constexpr auto call(A B::*pmd, T&& t)
+        noexcept(noexcept((get)(static_cast<T&&>(t)).*pmd))
+        -> decltype((get)(static_cast<T&&>(t)).*pmd)
+    { return (get)(static_cast<T&&>(t)).*pmd; }
+};
+
+template<class F, class... Args>
+constexpr auto invoke(F&& f, Args&&... args)
+    noexcept(noexcept(invoke_impl<std::decay_t<F>>::call(
+            static_cast<F&&>(f), static_cast<Args&&>(args)...)))
+    -> decltype(invoke_impl<std::decay_t<F>>::call(
+            static_cast<F&&>(f), static_cast<Args&&>(args)...))
 {
-    return InvokeSelector<std::is_member_pointer_v<std::decay_t<F>>>::call(
-        std::forward<F>(f), std::forward<ArgTypes>(args)...);
+    return invoke_impl<std::decay_t<F>>::call(
+        static_cast<F&&>(f), static_cast<Args&&>(args)...);
 }
 
-template<class R, class F, class... ArgTypes,
-    std::enable_if_t<!std::is_void_v<R>, int> = 0,
-    std::enable_if_t<
-        std::is_convertible_v<
-            decltype(
-                InvokeSelector<std::is_member_pointer_v<std::decay_t<F>>>::call(
-                    std::declval<F>(), std::declval<ArgTypes>()...)),
-            R
-        >,
-        int> = 0
+template<class T>
+void implicit_cast(T) noexcept;
+
+template<class R, class F, class... Args,
+    class = std::enable_if_t<!std::is_void_v<R>>,
+    class = decltype((implicit_cast<R>)(
+        invoke_impl<std::decay_t<F>>::call(
+            std::declval<F>(), std::declval<Args>()...)))
 >
-constexpr R invoke(F&& f, ArgTypes&&... args) {
-    return InvokeSelector<std::is_member_pointer_v<std::decay_t<F>>>::call(
-        std::forward<F>(f), std::forward<ArgTypes>(args)...);
+constexpr R invoke(F&& f, Args&&... args)
+    noexcept(noexcept((implicit_cast<R>)(
+        invoke_impl<std::decay_t<F>>::call(
+            static_cast<F&&>(f), static_cast<Args&&>(args)...))))
+{
+    return invoke_impl<std::decay_t<F>>::call(
+        static_cast<F&&>(f), static_cast<Args&&>(args)...);
 }
 
-template<class R, class F, class... ArgTypes,
-    std::enable_if_t<std::is_void_v<R>, int> = 0,
-    decltype((void)InvokeSelector<std::is_member_pointer_v<std::decay_t<F>>>::call(
-        std::declval<F>(), std::declval<ArgTypes>()...), 0) = 0
+template<class R, class F, class... Args,
+    class = std::enable_if_t<std::is_void_v<R>>,
+    class = decltype(invoke_impl<std::decay_t<F>>::call(
+        std::declval<F>(), std::declval<Args>()...))
 >
-constexpr void invoke(F&& f, ArgTypes&&... args) {
-    InvokeSelector<std::is_member_pointer_v<F>>::call(
-        std::forward<F>(f), std::forward<ArgTypes>(args)...);
+constexpr void invoke(F&& f, Args&&... args)
+    noexcept(noexcept(invoke_impl<std::decay_t<F>>::call(
+        static_cast<F&&>(f), static_cast<Args&&>(args)...)))
+{
+    invoke_impl<std::decay_t<F>>::call(
+        static_cast<F&&>(f), static_cast<Args&&>(args)...);
 }
